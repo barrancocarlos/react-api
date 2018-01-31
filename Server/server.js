1fcddfb27f51ca7bf3858b17021eb39e4f877b97
Carlos <barrancocarlos@hotmail.com>
2018-01-30 21:25:32 -0400

    swagger

--- Server/server.js
+++ Server/server.js
@@ -6,6 +6,8 @@ var morgan = require('morgan');             // log requests to the console (expr
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
 var config = require('./config/config');
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('./swagger.json');
 
 // Db connect =================
 require('./config/database');
@@ -19,6 +21,7 @@ app.use(morgan('dev'));                                         // log every req
 app.use(bodyParser.urlencoded({'extended':'true'}));            // parse application/x-www-form-urlencoded
 app.use(bodyParser.json());                                     // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(methodOverride('_method'));              // put and delete
 app.set('view engine', 'ejs');                                  // template engine
 //app.engine('.html', require('ejs').__express);                change views from .ejs to .html
